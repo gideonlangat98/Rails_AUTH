8254046dff432eff0d4b809aa535eee939e0c930
gideonlangat98 <gideonlangat98@gmail.com>
2022-10-19 23:12:27 +0300

    Initializing repository

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  root "static_pages#home"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  resouces :confirmation, only: [:create, :edit, :new], param: :confirmation_token
+end
